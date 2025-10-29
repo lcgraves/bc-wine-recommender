b0db700c66d1d92b1734b5936a900cce2196fb4a
lcgraves <lgraves@northislandcollege.ca>
2025-10-29 10:48:31 -0700

    Use conditional logic to show active page

--- Public/partials/nav.php
+++ Public/partials/nav.php
@@ -5,8 +5,8 @@
             <a href="index.php" class="logo">The BC Pour</a>
             <!-- Navigation Links -->
             <nav class="nav">
-                <a href="index.php" class="nav-link current">Home</a>
-                <a href="about.php" class="nav-link">About</a>
+                <a href="index.php" class="<?php if ($_SERVER['REQUEST_URI'] == "/bc-wine-recommender/Public/index.php") { echo 'current';} ?> nav-link ">Home</a>
+                <a href="about.php" class="<?php if ($_SERVER['REQUEST_URI'] == "/bc-wine-recommender/Public/about.php") { echo 'current';} ?> nav-link">About</a>
                 <a href="login.php" class="admin-button">Admin Login</a>
             </nav>
         </div>

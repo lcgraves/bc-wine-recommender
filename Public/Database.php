6b65d0813ea5178cb651253bc0f81a8a2a2f5164
lcgraves <lgraves@northislandcollege.ca>
2025-10-30 11:11:52 -0700

    Remove username since parameter in new PDO

--- Public/Database.php
+++ Public/Database.php
@@ -3,7 +3,7 @@
 class Database {
     public $connection;
     public function __construct() {
-    $dsn = "mysql:host=localhost;port=3306;dbname=wine_db;user=root;charset=utf8mb4";
+    $dsn = "mysql:host=localhost;port=3306;dbname=wine_db;charset=utf8mb4";
     $this->connection = new PDO($dsn, 'root', '', [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);

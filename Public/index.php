0615dcb5f360f98c4e2c75e57486b05d756fe907
lcgraves <lgraves@northislandcollege.ca>
2025-10-29 18:34:36 -0700

    Connect to MySQL database 'wine_db'

--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,14 @@
 <?php
 require 'functions.php';
 require 'views/index.view.php';
+
+// connect to MySQL wine_db
+
+$dsn = "mysql:host=localhost;port=3306;dbname=wine_db;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM `wine_db`.`wine-data`");
+$statement->execute();
+$wines = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($wines);

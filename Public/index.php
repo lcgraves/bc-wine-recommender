bd61de5d5d76d90ff894ad40414367d3e320ac9e
lcgraves <lgraves@northislandcollege.ca>
2025-10-29 18:50:54 -0700

    Display list of wines in database

--- Public/index.php
+++ Public/index.php
@@ -2,13 +2,15 @@
 require 'functions.php';
 require 'views/index.view.php';
 
-// connect to MySQL wine_db
+// connect to MySQL wine_db and execute query
 
 $dsn = "mysql:host=localhost;port=3306;dbname=wine_db;user=root;charset=utf8mb4";
 $pdo = new PDO($dsn);
 
 $statement = $pdo->prepare("SELECT * FROM `wine_db`.`wine-data`");
 $statement->execute();
-$wines = $statement->fetchAll(PDO::FETCH_ASSOC);
+$wines = $statement->fetchAll(PDO::FETCH_ASSOC); // gives results as associative array
 
-dd($wines);
+foreach ($wines as $wine) {
+    echo "<li> " . $wine['name'] . " - " . $wine['winery'] . " - " . $wine['region'] . " - " . $wine['colour'] . " - " . $wine['price'] . "</li>";
+}

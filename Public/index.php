cacf9f22157542a2f55fba269d13b4586e179eed
lcgraves <lgraves@northislandcollege.ca>
2025-10-29 19:08:25 -0700

    Place database class and methods in its own file

--- Public/index.php
+++ Public/index.php
@@ -1,15 +1,13 @@
 <?php
 require 'functions.php';
 require 'views/index.view.php';
+require 'Database.php';
+
 
 // connect to MySQL wine_db and execute query
 
-$dsn = "mysql:host=localhost;port=3306;dbname=wine_db;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("SELECT * FROM `wine_db`.`wine-data`");
-$statement->execute();
-$wines = $statement->fetchAll(PDO::FETCH_ASSOC); // gives results as associative array
+$db = new Database();
+$wines = $db->query("SELECT * FROM `wine_db`.`wine-data`")->fetchAll(PDO::FETCH_ASSOC); 
 
 foreach ($wines as $wine) {
     echo "<li> " . $wine['name'] . " - " . $wine['winery'] . " - " . $wine['region'] . " - " . $wine['colour'] . " - " . $wine['price'] . "</li>";

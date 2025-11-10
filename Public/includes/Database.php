aecdb76dacc379d0a583bf3d531a1969470d56aa
lcgraves <lgraves@northislandcollege.ca>
2025-11-10 12:46:20 -0800

    Add a 'create database connection' function

--- Public/includes/Database.php
+++ Public/includes/Database.php
@@ -1,20 +1,30 @@
 <?php
 
-class Database {
-    public $connection;
-    public function __construct($config, $username = 'root', $password = '') {
+/* database connection */
+require_once 'config.php';  
 
-    $dsn = 'mysql:' . http_build_query($config, '', ';'); // to build $dsn string
 
-    $this->connection = new PDO($dsn, $username, $password, [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
-}
-    public function query($query) {
+/* create database connection */
 
-    $statement = $this->connection->prepare($query);
-    $statement->execute();
+function createDBConnection() {
+    /* $pdo is static to ensure a single connection instance */
+    static $pdo = null;
 
-    return $statement;
+    if ($pdo === null) {
+
+    $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // throws php exception on database error
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //sets default way to retrieve data as associative array
+        PDO::ATTR_EMULATE_PREPARES => false, // use database's native prepared SQL statements if supported
+    ];
+
+    try {
+        $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+    } catch (PDOException $e) {
+        die('Database connection failed: ' . $e->getMessage());
     }
+}
+
+return $pdo;
 }

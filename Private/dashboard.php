ab7361104b9cef1eb723f4c7e9d314e951981788
lcgraves <lgraves@northislandcollege.ca>
2025-11-24 13:48:10 -0800

    Redirect user if not logged in successfully

--- Private/dashboard.php
+++ Private/dashboard.php
@@ -4,6 +4,11 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Redirect user if not logged in successfully
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    require_once '../Public/includes/Database.php'; // Need redirect() function
+    redirect('../Public/login.php');
+}
 
 $page_title = "Admin Dashboard";
 require_once '../Public/includes/Database.php';

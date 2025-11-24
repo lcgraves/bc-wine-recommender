8684d996b24c73db91b8b094c4740c7637ffa82d
lcgraves <lgraves@northislandcollege.ca>
2025-11-23 16:13:40 -0800

    Create process_filters.php and start session

---
+++ Public/includes/process_filters.php
@@ -0,0 +1,10 @@
+<?php
+// ===================================
+// 1. SETUP
+// ===================================
+
+// Start the session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+

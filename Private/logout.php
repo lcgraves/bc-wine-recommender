b2a8c37f807d4dbfe14d83f7b4d6ec0c10dc71d2
lcgraves <lgraves@northislandcollege.ca>
2025-11-25 13:44:30 -0800

    Clear session array

--- Private/logout.php
+++ Private/logout.php
@@ -2,8 +2,11 @@
 // Access session data
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-
+}
 require_once 'includes/Database.php';
 
+// --- 1. TERMINATE SESSION ---
 
-}
+// 1. Clear $_SESSION array
+$_SESSION = [];
+

6cf905a8c8daff854d04e356c34ffc706b9f6343
lcgraves <lgraves@northislandcollege.ca>
2025-11-26 10:43:38 -0800

    Initialize wine and flavour notes variables

--- Private/edit-wine.php
+++ Private/edit-wine.php
@@ -16,10 +16,23 @@ $page_title = "Edit Wine";
 require 'includes/header_private.php';
 require 'includes/side_nav.php';
 $message = ''; // For success or error messages
-$wine_id = $_GET['id'] ?? null; // Get wine ID from URL
+$wine_id = $_GET['id'] ?? null; // Get wine ID from URL for intial load
 
 // If no ID is provided in the URL, redirect back to the manage page
 if (!$wine_id) {
     redirect('manage-wines.php');
 }
 
+// Initialize variables (will be filled below)
+$wine = [];
+$tasting_notes_db = []; // notes currently in database for this wine
+$selected_notes = []; // Notes selected by user in the form
+
+
+?>
+
+<section>
+
+        
+</section>
+<?php require 'includes/footer.php' ?>

cddcc6aab519be24608fa177a2e8e677f38d0d2d
lcgraves <lgraves@northislandcollege.ca>
2025-11-26 15:42:41 -0800

    Create register.php and setup file

---
+++ Public/register.php
@@ -0,0 +1,12 @@
+<?php
+
+$page_title = "Login";
+require_once 'includes/Database.php';
+require 'includes/header.php';
+require 'includes/nav.php';
+
+?>
+
+<main class="container login-wrapper">
+        <div class="login-card">
+            <h1 class="recommender-title" style="font-size: 2rem; margin-bottom: 2rem;">Register New Admin</h1>

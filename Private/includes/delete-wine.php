e58dcce3292e045b13761fe04b60193f61ee82dd
lcgraves <lgraves@northislandcollege.ca>
2025-11-26 14:50:18 -0800

    Get valid wine id or redirect back to manage wines

--- Private/includes/delete-wine.php
+++ Private/includes/delete-wine.php
@@ -10,3 +10,11 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
 }
 
 $pdo = createDBConnection();
+
+// 1. Get the wine ID from the POST request
+$wine_id = filter_input(INPUT_POST, 'wine_id', FILTER_VALIDATE_INT);
+
+// If no valid ID is present, or if it wasn't a POST request, redirect back
+if (!$wine_id) {
+    redirect('manage_wines.php');
+}

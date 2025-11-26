0ac3e9ecb15753178dce2af2a8ecce9292d9f643
lcgraves <lgraves@northislandcollege.ca>
2025-11-25 16:11:38 -0800

    Create edit-wine.php, start session & check if user logged in

---
+++ Private/edit-wine.php
@@ -0,0 +1,13 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once 'includes/Database.php'; 
+$pdo = createDBConnection();
+
+// Check for authenticated user (Redirect if not logged in)
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    redirect('../Public/login.php');
+}

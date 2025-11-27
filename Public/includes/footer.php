6c943dde23248cd19707449f2ae61a372c72a077
lcgraves <lgraves@northislandcollege.ca>
2025-11-26 17:53:23 -0800

    Add link to bootstrap javascript

--- Public/includes/footer.php
+++ Public/includes/footer.php
@@ -4,6 +4,7 @@
             &copy; 2025 The BC Pour | Supporting BC Wine.
         </div>
     </footer>
-    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>

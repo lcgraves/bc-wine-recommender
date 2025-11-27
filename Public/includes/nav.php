df132d4568a04237cf4d9dd3c449012fd9ecc00b
lcgraves <lgraves@northislandcollege.ca>
2025-11-27 08:40:05 -0800

    Make nav responsive

--- Public/includes/nav.php
+++ Public/includes/nav.php
@@ -1,13 +1,31 @@
 <!-- Header & Navigation -->
-    <header class="header">
-        <div class="container header-content">
-            <!-- Logo/Title -->
-            <a href="index.php" class="logo">The BC Pour</a>
-            <!-- Navigation Links -->
-            <nav class="nav">
-                <a href="index.php" class="<?= urlIs('/bc-wine-recommender/Public/index.php') ? 'current' : ''; ?> nav-link ">Home</a>
-                <a href="about.php" class="<?= urlIs('/bc-wine-recommender/Public/about.php') ? 'current' : ''; ?> nav-link">About</a>
-                <a href="login.php" class="admin-button">Admin Login</a>
-            </nav>
+<header class="header navbar navbar-expand-md navbar-dark">
+    <div class="container header-content">
+        <a href="index.php" class="navbar-brand logo">The BC Pour</a> 
+        <button
+            class="navbar-toggler"          
+            type="button"
+            data-bs-toggle="collapse"       
+            data-bs-target="#navbarNav"     
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link <?= urlIs('/bc-wine-recommender/Public/index.php') ? 'active' : ''; ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.php" class="nav-link <?= urlIs('/bc-wine-recommender/Public/about.php') ? 'active' : ''; ?>">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link admin-button">Admin Login</a> 
+                </li>
+            </ul>
         </div>
-    </header>
+    </div>
+</header>

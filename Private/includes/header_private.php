deabc44c2483f478479903545a7e9a43386ce87e
lcgraves <lgraves@northislandcollege.ca>
2025-11-26 17:51:33 -0800

    Add link to bootstrap

--- Private/includes/header_private.php
+++ Private/includes/header_private.php
@@ -6,6 +6,7 @@
     <title><?= html_escape($page_title) ?> - <?= SITE_NAME ?></title>
     <link rel="stylesheet" href="/bc-wine-recommender/Public/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="bg-wine-bg">
 
--- Public/includes/header.php
+++ Public/includes/header.php
@@ -8,5 +8,6 @@
     <link rel="stylesheet" href="styles.css">
     <!-- Load Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="bg-wine-bg">

17ffd061127de1f8e3655e3dc31a7c2743adcb30
lcgraves <lgraves@northislandcollege.ca>
2025-11-27 18:05:23 -0800

    Format file

--- Public/includes/config.php
+++ Public/includes/config.php
@@ -5,12 +5,12 @@
 
 /* Database credentials */
 
-define ('DB_HOST', 'localhost');
-define ('DB_PORT', 3306);
-define ('DB_NAME', 'wine_db');
-define ('DB_CHARSET', 'utf8mb4');  
-define ('DB_PASS', '');
-define ('DB_USER', 'root');  
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_NAME', 'wine_db');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_PASS', '');
+define('DB_USER', 'root');
 
 /* Application settings */
 
@@ -19,9 +19,9 @@ define('BASE_URL', 'http://localhost/bc-wine-recommender/Public/');
 define('UPLOAD_PATH', __DIR__ . '/../images/');
 
 /* Image upload settings */
- // Define allowed extensions and max size
-        $allowed_extensions = ['jpg', 'jpeg', 'png', 'webp', 'avif'];
-        $max_size = 5 * 1024 * 1024; // 5MB
+// Define allowed extensions and max size
+$allowed_extensions = ['jpg', 'jpeg', 'png', 'webp', 'avif'];
+$max_size = 5 * 1024 * 1024; // 5MB
 
 /* Error reporting for production environment */
 
